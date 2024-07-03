af7421003159feae03be43e8f7c3dec4a8286f1e
Erika Dickson <ErikaLD@csu.fullerton.edu>
2024-07-03 10:07:44 -0700

    cleaned up main.cpp and deleted unnecessary files (will be reworked)

--- main.cpp
+++ main.cpp
@@ -22,11 +22,9 @@ int main()
 
     // Reading in csv file values for Lenovo and inputting it into corresponding parallel arrays
     // Open the CSV file
-
     ifstream lenovo_file("LNVGF.csv");
-    ifstream motorola_file("MSI.csv");
+    //ifstream motorola_file("MSI.csv");
 
-   // -----------------------------------------------------------
 
     Stock lenovo;
 
@@ -38,50 +36,7 @@ int main()
     lenovo.SetAdjClosesVec(lenovo_file);
     lenovo.SetVolumesVec(lenovo_file);
 
-    //vector<string> temp = lenovo.GetDatesVec();
-    //cout << "\nempty? " << temp.empty() << endl;
-    //cout << temp.at(0);
-
-    // cout << "\n\n";
-
     lenovo.SetStockRecords();
-
-    // lenovo.SetOpensVec(lenovo_file);
-    // for (int j = 0; j < 15; ++j)
-    // {
-    //     cout << lenovo.GetOpensVec()[j] << " ";
-    // }
-    // cout << "\n\n";
-
-    // for (int i = 0; i < MAX_ROWS; ++i)
-    // {
-    //     cout << lenovo.GetHighs()[i] << " ";
-    // }
-    // cout << "\n\n";
-
-    // for (int i = 0; i < MAX_ROWS; ++i)
-    // {
-    //     cout << lenovo.GetLows()[i] << " ";
-    // }
-    // cout << "\n\n";
-
-    // for (int i = 0; i < MAX_ROWS; ++i)
-    // {
-    //     cout << lenovo.GetCloses()[i] << " ";
-    // }
-    // cout << "\n\n";
-
-    // for (int i = 0; i < MAX_ROWS; ++i)
-    // {
-    //     cout << lenovo.GetAdjCloses()[i] << " ";
-    // }
-    // cout << "\n\n";
-
-    // for (int i = 0; i < MAX_ROWS; ++i)
-    // {
-    //     cout << lenovo.GetVolumes()[i] << " ";
-    // }
-    // cout << "\n\n";
 
     // Declared & initialized boolean variables to be used to flag when there is opportunity for exercising pairs trading concepts
     // trade_flag will be raised (set to true) when a certain threshold of separation between stock values is reached
